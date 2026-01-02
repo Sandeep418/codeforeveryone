ab82d7176cbcdf7a87c0fb66653ff891b7efed03
Sandeep G <40691260+Sandeep418@users.noreply.github.com>
2026-01-02 19:35:52 +0530

    Update Twosum.py

--- Twosum.py
+++ Twosum.py
@@ -6,3 +6,15 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+
+#Hashmap
+
+   seen={}
+
+    for i, num in enumerate(nums):
+        need = target - num
+        if need in seen:
+            return [seen[need], i]
+        seen[num] = i
