1b1db54626a2dcb3cd9147f383e61b4b4d2cd7c2
Sandeep G <40691260+Sandeep418@users.noreply.github.com>
2026-01-03 17:49:55 +0530

    Add function to square and sort array elements
    Implement a function to return squares of a sorted array.

---
+++ squares_of_sorted_array.py
@@ -0,0 +1,19 @@
+'''
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Examples:
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100]. After sorting, it becomes [0,1,9,16,100].
+'''
+
+class Solution:
+    def squaresOfASortedArray(self, nums: List[int]) -> List[int]:
+        results=[]
+        for num in nums:
+            n=num*num
+            results.append(n)
+        results.sort()
+        return results
