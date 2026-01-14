b3db0d6f09defa58da27422c680cebfcfdb95368
Sandeep G <40691260+Sandeep418@users.noreply.github.com>
2026-01-14 06:39:38 +0530

    Optimize square sorting with two-pointer approach
    Refactor to use two-pointer technique for efficiency.

--- squares_of_sorted_array.py
+++ squares_of_sorted_array.py
@@ -15,5 +15,32 @@ class Solution:
         for num in nums:
             n=num*num
             results.append(n)
-        results.sort()
+        results.sort()  // o(n log(n))
         return results
+
+// o(n)
+from typing import List
+
+class Solution:
+    def squaresOfASortedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+
+        left = 0
+        right = n - 1
+        pos = n - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[pos] = nums[left] * nums[left]
+                left += 1
+            else:
+                result[pos] = nums[right] * nums[right]
+                right -= 1
+            pos -= 1
+
+        return result
+
+
+
+
